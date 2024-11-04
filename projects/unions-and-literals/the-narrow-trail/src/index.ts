cf924badd5eb7d9cb326dd86f0efc0a9fa30338a
Victor Volovik <VictorVolovik@users.noreply.github.com>
2024-11-04 15:25:02 +0400

    Complete Chapter 3 exercises

--- projects/unions-and-literals/the-narrow-trail/src/index.ts
+++ projects/unions-and-literals/the-narrow-trail/src/index.ts
@@ -1,3 +1,64 @@
 export function runCommands() {
-	// Declare your variables and runtime logic here! ✨
+	let availableResource: "food" | "water" | undefined;
+	let day = 1;
+	let food = 5;
+	let water = 5;
+
+	while (day <= 7) {
+		const rolled = Math.floor(Math.random() * 6) + 1;
+
+		// Random events logic
+		switch (rolled) {
+			case 1:
+				availableResource = "food";
+				break;
+			case 2:
+				availableResource = "water";
+				break;
+			case 3:
+			case 5:
+				if (!availableResource) {
+					availableResource = "water";
+				} else {
+					if (availableResource === "water") {
+						water += rolled;
+					}
+					if (availableResource === "food") {
+						food += rolled;
+					}
+					availableResource = undefined;
+				}
+				break;
+			case 4:
+			case 6:
+				if (!availableResource) {
+					availableResource = "food";
+				} else {
+					if (availableResource === "water") {
+						water += rolled;
+					}
+					if (availableResource === "food") {
+						food += rolled;
+					}
+					availableResource = undefined;
+				}
+				break;
+		}
+
+		// Daily expenses
+		food--;
+		water--;
+
+		// Supply check
+		if (food === 0 || water === 0) {
+			// We lost
+			return false;
+		}
+
+		// Advance to next day
+		day++;
+	}
+
+	// We survived 7 days
+	return true;
 }
